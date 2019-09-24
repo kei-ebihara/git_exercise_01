12ae6f11e2437c1645e15b8a72e62fb1d64c5ad1
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2019-09-24 18:49:41 +0900

    合計値算出メソッド作成

--- 01/ex01.rb
+++ 01/ex01.rb
@@ -1,5 +1,7 @@
 sales = { "hokkaido" => 150, "tohoku" => 200, "kanto" => 500, "chubu" => 300, "kinki" => 400, "chugoku" => 180, "shukoku" => 140, "kyushu" => 120}
 
 sales.each do |name, sale|
-puts "#{name}"
+  puts "#{name}"
 end
+
+puts "#{sales.values.inject(:+)}"

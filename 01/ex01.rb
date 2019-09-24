5a900f94cc143d779bf7b823cb33af9ed186d173
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2019-09-24 18:05:19 +0900

    name出力メソッド作成

--- 01/ex01.rb
+++ 01/ex01.rb
@@ -1,1 +1,5 @@
 sales = { "hokkaido" => 150, "tohoku" => 200, "kanto" => 500, "chubu" => 300, "kinki" => 400, "chugoku" => 180, "shukoku" => 140, "kyushu" => 120}
+
+sales.each do |name, sale|
+puts "#{name}"
+end

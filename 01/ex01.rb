96f0b810c8eb4a4a7992f4e5d6b8c140c5fa3b91
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2019-09-25 23:21:31 +0900

    部署名抽出メソッド修正

--- 01/ex01.rb
+++ 01/ex01.rb
@@ -1,5 +1,8 @@
-sales = { "hokkaido" => 150, "tohoku" => 200, "kanto" => 500, "chubu" => 300, "kinki" => 400, "chugoku" => 180, "shukoku" => 140, "kyushu" => 120}
-
-sales.each do |name, sale|
-puts "#{name}"
+def sales_name
+  sales = { "hokkaido" => 150, "tohoku" => 200, "kanto" => 500, "chubu" => 300, "kinki" => 400, "chugoku" => 180, "shukoku" => 140, "kyushu" => 120}
+  sales.each do |name, sale|
+  puts "#{name}"
+  end
 end
+
+sales_name

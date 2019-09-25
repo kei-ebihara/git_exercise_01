a40eb6460c95ac565e64ead65c947070de7bef98
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2019-09-25 22:59:04 +0900

    赤字部署抽出メソッド作成

--- 01/ex01.rb
+++ 01/ex01.rb
@@ -1,5 +1,19 @@
-sales = { "hokkaido" => 150, "tohoku" => 200, "kanto" => 500, "chubu" => 300, "kinki" => 400, "chugoku" => 180, "shukoku" => 140, "kyushu" => 120}
-
-sales.each do |name, sale|
-puts "#{name}"
+def sales_name
+  sales = { "hokkaido" => 150, "tohoku" => 200, "kanto" => 500, "chubu" => 300, "kinki" => 400, "chugoku" => 180, "shukoku" => 140, "kyushu" => 120}
+  sales.each do |name, sale|
+  puts "#{name}"
+  end
 end
+
+sales_name
+
+
+def damesyainn
+  sales = { "hokkaido" => 150, "tohoku" => 200, "kanto" => 500, "chubu" => 300, "kinki" => 400, "chugoku" => 180, "shukoku" => 140, "kyushu" => 120}
+  small_sales = sales.select {|name,sale| sale <= 300}
+  p small_sales.length
+end
+
+damesyainn
+
+

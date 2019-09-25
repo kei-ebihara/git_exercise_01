397c4e26dcebdfaf2d603179ffa9b9f1f340966c
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2019-09-25 23:28:51 +0900

    総売上算出メソッド修正

--- 01/ex01.rb
+++ 01/ex01.rb
@@ -1,7 +1,15 @@
-sales = { "hokkaido" => 150, "tohoku" => 200, "kanto" => 500, "chubu" => 300, "kinki" => 400, "chugoku" => 180, "shukoku" => 140, "kyushu" => 120}
+# sales = { "hokkaido" => 150, "tohoku" => 200, "kanto" => 500, "chubu" => 300, "kinki" => 400, "chugoku" => 180, "shukoku" => 140, "kyushu" => 120}
 
-sales.each do |name, sale|
-  puts "#{name}"
+# sales.each do |name, sale|
+#   puts "#{name}"
+# end
+
+
+
+def souuriage
+  sales = { "hokkaido" => 150, "tohoku" => 200, "kanto" => 500, "chubu" => 300, "kinki" => 400, "chugoku" => 180, "shukoku" => 140, "kyushu" => 120}
+  
+  puts "#{sales.values.inject(:+)}"
 end
 
-puts "#{sales.values.inject(:+)}"
+souuriage

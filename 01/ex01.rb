2f3aee5ed9b51eadd3bbb091d043a3a45b9f4df3
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2019-09-26 22:16:01 +0900

    部署一覧抽出メソッド修正

--- 01/ex01.rb
+++ 01/ex01.rb
@@ -1,7 +1,7 @@
 def sales_name
-  sales = { "hokkaido" => 150, "tohoku" => 200, "kanto" => 500, "chubu" => 300, "kinki" => 400, "chugoku" => 180, "shukoku" => 140, "kyushu" => 120}
+  sales = { :hokkaido => 150, :tohoku => 200, :kanto => 500, :chubu => 300, :kinki => 400, :chugoku => 180, :shukoku => 140, :kyushu => 120}
   sales.each do |name, sale|
-  puts "#{name}"
+    puts "#{name}"
   end
 end
 

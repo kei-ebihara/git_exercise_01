4805acfd670b8c551adf11507e49c5f220e24927
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2019-09-26 23:22:56 +0900

    総売上メソッド修正2

--- 01/ex01.rb
+++ 01/ex01.rb
@@ -9,8 +9,12 @@
 
 # sales = { :hokkaido => 150, :tohoku => 200, :kanto => 500, :chubu => 300, :kinki => 400, :chugoku => 180, :shukoku => 140, :kyushu => 120}
 
+
+
 def souuriage(sales)
   puts "#{sales.values.inject(:+)}"
 end
 
 souuriage( :hokkaido => 150, :tohoku => 200, :kanto => 500, :chubu => 300, :kinki => 400, :chugoku => 180, :shukoku => 140, :kyushu => 120)
+
+

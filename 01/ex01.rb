272ea789a3e2d01b414b9c863d0b3374d15f0695
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2019-09-26 22:59:48 +0900

    総売上メソッド修正

--- 01/ex01.rb
+++ 01/ex01.rb
@@ -1,15 +1,16 @@
-# sales = { "hokkaido" => 150, "tohoku" => 200, "kanto" => 500, "chubu" => 300, "kinki" => 400, "chugoku" => 180, "shukoku" => 140, "kyushu" => 120}
-
-# sales.each do |name, sale|
-#   puts "#{name}"
+# def sales_name
+#   sales = { :hokkaido => 150, :tohoku => 200, :kanto => 500, :chubu => 300, :kinki => 400, :chugoku => 180, :shukoku => 140, :kyushu => 120}
+#   sales.each do |name, sale|
+#     puts "#{name}"
+#   end
 # end
 
+# sales_name
 
+# sales = { :hokkaido => 150, :tohoku => 200, :kanto => 500, :chubu => 300, :kinki => 400, :chugoku => 180, :shukoku => 140, :kyushu => 120}
 
-def souuriage
-  sales = { "hokkaido" => 150, "tohoku" => 200, "kanto" => 500, "chubu" => 300, "kinki" => 400, "chugoku" => 180, "shukoku" => 140, "kyushu" => 120}
-  
+def souuriage(sales)
   puts "#{sales.values.inject(:+)}"
 end
 
-souuriage
+souuriage( :hokkaido => 150, :tohoku => 200, :kanto => 500, :chubu => 300, :kinki => 400, :chugoku => 180, :shukoku => 140, :kyushu => 120)

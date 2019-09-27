6ad4709e07294b90f6f228ab0ca6657e6c3d9a0b
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2019-09-27 19:35:52 +0900

    激詰め部署抽出メソッド修正

--- 01/ex01.rb
+++ 01/ex01.rb
@@ -1,19 +1,15 @@
-def sales_name
-  sales = { "hokkaido" => 150, "tohoku" => 200, "kanto" => 500, "chubu" => 300, "kinki" => 400, "chugoku" => 180, "shukoku" => 140, "kyushu" => 120}
-  sales.each do |name, sale|
-  puts "#{name}"
-  end
-end
+# def souuriage(sales)
+#   puts "#{sales.values.inject(:+)}"
+# end
 
-sales_name
+# souuriage( :hokkaido => 150, :tohoku => 200, :kanto => 500, :chubu => 300, :kinki => 400, :chugoku => 180, :shukoku => 140, :kyushu => 120)
 
 
-def damesyainn
-  sales = { "hokkaido" => 150, "tohoku" => 200, "kanto" => 500, "chubu" => 300, "kinki" => 400, "chugoku" => 180, "shukoku" => 140, "kyushu" => 120}
+def damesyainn(sales)
   small_sales = sales.select {|name,sale| sale <= 300}
   p small_sales.length
 end
 
-damesyainn
+damesyainn( :hokkaido => 150, :tohoku => 200, :kanto => 500, :chubu => 300, :kinki => 400, :chugoku => 180, :shukoku => 140, :kyushu => 120)
 
 

e54df1c3da53d856ba0dfb35321139153c9de03f
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2019-09-28 22:30:16 +0900

    激詰め部署算出メソッド修正XYZ

--- 01/ex01.rb
+++ 01/ex01.rb
@@ -1,15 +1,27 @@
+# # def sales_name
+# #   sales = { :hokkaido => 150, :tohoku => 200, :kanto => 500, :chubu => 300, :kinki => 400, :chugoku => 180, :shukoku => 140, :kyushu => 120}
+# #   sales.each do |name, sale|
+# #     puts "#{name}"
+# #   end
+# # end
+
+# # sales_name
+
+
+# sales = { :hokkaido => 150, :tohoku => 200, :kanto => 500, :chubu => 300, :kinki => 400, :chugoku => 180, :shukoku => 140, :kyushu => 120}
 
 # def souuriage(sales)
 #   puts "#{sales.values.inject(:+)}"
 # end
 
-# souuriage( :hokkaido => 150, :tohoku => 200, :kanto => 500, :chubu => 300, :kinki => 400, :chugoku => 180, :shukoku => 140, :kyushu => 120)
+# souuriage(sales)
 
+
+sales = { :hokkaido => 150, :tohoku => 200, :kanto => 500, :chubu => 300, :kinki => 400, :chugoku => 180, :shukoku => 140, :kyushu => 120}
 
 def damesyainn(sales)
   small_sales = sales.select {|name,sale| sale <= 300}
   p small_sales.length
 end
 
-damesyainn( :hokkaido => 150, :tohoku => 200, :kanto => 500, :chubu => 300, :kinki => 400, :chugoku => 180, :shukoku => 140, :kyushu => 120)
-
+damesyainn(sales)
